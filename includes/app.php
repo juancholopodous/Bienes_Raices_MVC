5979e6a6947f44a80e388a9f83f337be6d5811b7
Juan <juanandresroth2@gmail.com>
2026-04-30 16:04:59 -0300

    cambio

--- includes/app.php
+++ includes/app.php
@@ -1,11 +1,9 @@
 <?php
-
-use Dotenv\Dotenv;
 use Model\ActiveRecord;
 
 require __DIR__ . '/../vendor/autoload.php'; // Agrega automaticamente las clases en este app.php
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';

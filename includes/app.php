0623872c732b5cb1e3716bf2f55dd1a37fb54c09
Juancholopodous <juanandresroth2@gmail.com>
2026-04-30 00:33:42 -0300

    prueba de ruta

--- includes/app.php
+++ includes/app.php
@@ -4,7 +4,8 @@ require __DIR__ . '/../vendor/autoload.php'; // Agrega automaticamente las clase
 use Dotenv\Dotenv;
 use Model\ActiveRecord;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
+//$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';

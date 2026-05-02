b0901e4b3b62d470cadd5dd39e96bac0919722d9
Juan <juanandresroth2@gmail.com>
2026-05-01 21:02:04 -0300

    Update

--- includes/app.php
+++ includes/app.php
@@ -1,18 +1,14 @@
-<?php
-use Dotenv\Dotenv;
-use Model\ActiveRecord;
-
-require __DIR__ . '/../vendor/autoload.php'; // Agrega automaticamente las clases en este app.php
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
-
+<?php 
 require 'funciones.php';
 require 'config/database.php';
+require __DIR__.'/../vendor/autoload.php'; // Agrega automaticamente las clases en este app.php
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
 // Conectar a la base de datos
 $db = conectarDB();
 
+use Model\ActiveRecord;
 
 //como es un atributo estatico NO requiere instanciarse
 ActiveRecord::setDB($db);
